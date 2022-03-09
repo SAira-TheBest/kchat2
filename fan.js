e4dfdd8e9f425ffaa59f0efed8aa19a3209c79ff
SAira-TheBest <82064943+SAira-TheBest@users.noreply.github.com>
2022-03-08 16:13:05 -0800

    Add files via upload

---
+++ fan.js
@@ -0,0 +1,31 @@
+status1 =""
+img =""
+function setup()
+{
+    canvas = createCanvas(640,420)
+    canvas.center()
+
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded)
+    document.getElementById("status").innerHTML = "Status: Detecting Objects"
+}
+
+function modelLoaded(){
+    console.log("Model loaded :)")
+    status1 = true
+    objectDetector.detect(img, gotResult)
+}
+
+function gotResult(error, results){
+ 
+    if(error){
+        console.error(error);
+    }
+    console.log(results)
+    Objects = results;
+}
+
+function preload()
+{
+    img = loadImage("USE THIS TOOOO.jpg")
+
+}
